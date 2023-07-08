bb83e67ed43316427044943e724d067caba79c00
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-08 01:19:58 -0700

    Task 13

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 num = 122
 while num > 96:
-    print(chr(num) if num % 2 == 0 else chr(num - 32), end="")
+    print("{}".format(chr(num) if num % 2 == 0 else chr(num - 32)), end="")
     num -= 1
