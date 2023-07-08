bf6612d1d4969b6abec7833d18ebcd692b3ff63e
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-08 04:18:53 -0700

    Task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """
-        imports the function def add(a, b): from the file add_0.py\
+        imports the function def add(a, b): from an external file\
                 and prints the result of the addition 1 + 2 = 3
     """
     from add_0 import add
