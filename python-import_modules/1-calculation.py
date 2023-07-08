846047ccd4405ad8ffc96d76187e8bbc44902200
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-08 04:29:33 -0700

    Task 1

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """
+        Imports functions from a module, does some Maths, and prints the result.
+    """
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
