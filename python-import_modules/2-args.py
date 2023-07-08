babcc98ff2b6d6d87db519df58a078263846e655
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-08 04:47:37 -0700

    Task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """
+        Outputs the number of and the list of its arguments.
+    """
+    from sys import argv
+    length = len(argv) - 1
+    if length == 0:
+        print("0 arguments.")
+    elif length === 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(length))
+    if length > 2:
+        for arg in range(1, length + 1):
+            print("{}".format(arg, argv[arg]))
