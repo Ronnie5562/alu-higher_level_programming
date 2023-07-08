7ff911b852352855508ae9b75678cd4cae4b4092
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-08 04:49:08 -0700

    Task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     length = len(argv) - 1
     if length == 0:
         print("0 arguments.")
-    elif length === 1:
+    elif length == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(length))
