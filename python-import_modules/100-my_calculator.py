034c36f01758d8ffbd34266f4005f5b76c26fa4b
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-09 08:28:01 -0700

    Task 6

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -7,12 +7,12 @@ if __name__ == "__main__":
     import sys
     length = len(sys.argv) - 1
     if length != 3:
-        print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
         operators = {"+": add, "-": sub, "*": mul, "/": div}
         if sys.argv[2] not in operators.keys():
-            print("{}".format("Unknown operator. Available operators: +, -, * and /"))
+            print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
         x = int(sys.argv[1])
         y = int(sys.argv[3])
