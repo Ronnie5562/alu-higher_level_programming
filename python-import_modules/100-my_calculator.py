6a2a89dfa95a68f1c99b15d90a1d7b0097a54baa
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-09 08:24:08 -0700

    Task 6

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """
+         imports all functions from a file and handles basic operations.
+    """
+    from calculator_1 import add, sub, mul, div
+    import sys
+    length = len(sys.argv) - 1
+    if length != 3:
+        print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
+        exit(1)
+    else:
+        operators = {"+": add, "-": sub, "*": mul, "/": div}
+        if sys.argv[2] not in operators.keys():
+            print("{}".format("Unknown operator. Available operators: +, -, * and /"))
+            exit(1)
+        x = int(sys.argv[1])
+        y = int(sys.argv[3])
+        z = sys.argv[2]
+        print("{} {} {} = {}".format(x, z, y, operators[z](x, y)))
