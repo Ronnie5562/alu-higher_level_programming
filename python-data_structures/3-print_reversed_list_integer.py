7cd4e69beb18821a039685a955e1c6955540c1c8
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-14 17:09:30 -0700

    Tasks

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for num in my_list[::-1]:
-        print("{:d}".format(num))
+    if len(my_list) > 0:
+        for num in my_list[::-1]:
+            print("{:d}".format(num))
