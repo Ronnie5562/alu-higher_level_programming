5d5f04a39f3f0bc9c202a24abb35b82c11575f5c
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-17 16:22:50 -0700

    Advanced Task

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    total_score = 0
+    num_of_val = 0
+    for score, weight in my_list:
+        total_score += (score * weight)
+        num_of_val += weight
+    return total_score / num_of_val
