d6fbad03d520e9668479e177f2b6f4368064e9b0
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-17 12:21:58 -0700

    Task 4

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(list_length):
+        try:
+            quotient = my_list_list[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            quotient = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            quotient = 0
+        except IndexError:
+            print("out of range")
+            quotient = 0
+        finally:
+            new_list.append(quotient)
+            return new_list
