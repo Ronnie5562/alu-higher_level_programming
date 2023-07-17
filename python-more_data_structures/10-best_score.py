c22937a49bd1e6f2d0ca23850027e62d89572958
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-17 03:47:16 -0700

    Task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-
+    if isinstance(a_dictionary, dict) and len(a_dictionary) > 0:
+        return max(a_dictionary, key=a_dictionary.get)
+    return None
