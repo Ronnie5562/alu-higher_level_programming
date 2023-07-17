cfeed2dd9d2983b720c865898f656ffacf6d6ff5
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-17 12:55:39 -0700

    Advanced Task

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except TypeError as TE:
-        print(f"Exception: {TE}", file=sys.stderr)
+    except (TypeError, ValueError) as TVE:
+        print(f"Exception: {TVE}", file=sys.stderr)
         return False
