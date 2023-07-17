3e88fcf7012f84bf6f221e13dafc3c2fc65b30cd
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-17 03:07:13 -0700

    Task 9

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+
--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return {k: a_dictionary[key] * 2 for key in a_dictionary}
+    return {key: a_dictionary[key] * 2 for key in a_dictionary}
