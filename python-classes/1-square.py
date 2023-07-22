eaa31966e96dcea87a29f5c369154f7df8e6b055
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-21 18:47:30 -0700

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """A class that defines a square"""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
