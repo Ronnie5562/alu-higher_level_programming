041dc7a747e4d51d4222ab368abbc6591ec2c5dd
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-22 14:01:53 -0700

    Task 6

---
+++ python-classes/6-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""A class Square that defines a square by: (based on 5-square.py)"""
+
+
+class Square:
+    """A class that defines a square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """property(get&set) for the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """property(get&set) for the current position of the square."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (isinstance(value, tuple) and len(value) == 2
+            and all(isinstance(val, int) for val in value)
+            and all(val >=0 for val in vaue)):
+            self.__position = value
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size**2)
+
+    def my_print(self):
+        """Prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print("")
+            return
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print()
