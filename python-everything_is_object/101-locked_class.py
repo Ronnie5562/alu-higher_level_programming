f7af0337fb81be3cc4f0e2854e4cb9c9ed9c8543
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-25 09:18:26 -0700

    Tasks

---
+++ python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module defines a class `LockedClass`."""
+
+
+class LockedClass:
+    """
+    prevents the user from dynamically creating new instance attributes, 
+    except if the new instance attribute is called first_name
+    """
+
+    __slots__ = ["first_name"]
