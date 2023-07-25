33ab2fcb47cce320e255ddc75bda0d53950ec533
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-25 09:32:51 -0700

    Tasks

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.num = getattr(magic_string, 'num', 0) + 1
-    return ("BestSchool, " * (magic_string.num - 1) + "BestSchool")
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
