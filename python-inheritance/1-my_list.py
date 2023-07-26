4a3293f2101ecbe4d08e545811ab70a00ad68f0f
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-26 10:43:57 -0700

    Task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Creates a Mylist class that inherits from list."""
+
+
+class MyList(list):
+    """Mylist class => A sub class of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
+    def print_sorted(self):
+        """prints the sorted list"""
+        print(self.sort())
