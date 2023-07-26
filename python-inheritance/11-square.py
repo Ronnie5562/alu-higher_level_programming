13077385df353bf7b47b688490a8ce2902ef0aa5
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-26 13:31:47 -0700

    Tasks

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -6,14 +6,6 @@ Rectangle = __import__('9-rectangle.py').Rectangle
 
 class Square(Rectangle):
     """A representation of a square, a special case of a rectangle with equal sides.
-
-    Attributes:
-        __size (int): The size (side length) of the square.
-
-    Methods:
-        __init__(self, size): Initialize a square instance with a given size.
-        area(self): Calculate and return the area of the square.
-        __str__(self): Return an informal string representation of the square.
     """
 
     def __init__(self, size):
@@ -26,19 +18,12 @@ class Square(Rectangle):
         super().__init__(size, size)
         self.__size = size
 
-    def area(self):
-        """Calculate and return the area of the square.
-
-        Returns:
-            int: The area of the square.
-        """
-        return self.__size ** 2
-
     def __str__(self):
         """Return an informal string representation of the square.
-
-        Returns:
-            str: A string representation of the square in the format '[Square] size/size'.
         """
         return f"[Square] {self.__size}/{self.__size}"
 
+    def area(self):
+        """Calculate and return the area of the square.
+        """
+        return self.__size ** 2
