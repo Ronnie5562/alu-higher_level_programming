8b6e1304a299e8fed3f1cce7222684875a231043
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-27 15:02:02 -0700

    Task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end="")
