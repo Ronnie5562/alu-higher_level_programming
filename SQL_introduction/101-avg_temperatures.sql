f4a629a9aec6314b836773017d511b90a03f94ab
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-30 09:55:11 -0700

    Task 18

---
+++ SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,2 @@
+-- Write a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
+SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures` GROUP BY `city` ORDER BY `avg_temp` DESC;
