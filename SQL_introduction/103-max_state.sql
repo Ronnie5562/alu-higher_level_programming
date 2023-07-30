df2b52cbbe3fda43d1e31611239101a5d969f69c
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-30 10:17:34 -0700

    Task 20

---
+++ SQL_introduction/103-max_state.sql
@@ -0,0 +1,2 @@
+-- Write a script that displays the max temperature of each state (ordered by State name).
+SELECT `state`, MAX(`value`) AS `max_temp` FROM `temperatures` GROUP BY `state` ORDER BY `state`;
