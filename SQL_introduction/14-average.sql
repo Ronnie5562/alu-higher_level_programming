a7dc7055d5331557cbc2868e2af6d155c3ee73b5
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-30 09:16:39 -0700

    Task 14

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- Write a script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- The result column name should be average
+SELECT AVG(`score`) AS `average` FROM `second_table`;
