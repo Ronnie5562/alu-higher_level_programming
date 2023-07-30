c6758339bad9a3f74400545e64ae9746007d3bf9
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-30 09:01:24 -0700

    Task 10

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
 -- Records should be ordered by score (top first)
-SELECT `score` `name` FROM `second_table` ORDEY BY `score` DESC;
+SELECT `score`, `name` FROM `second_table` ORDER BY `score` DESC;
