8edb1fe13ed60285d411fbfca54d758c488b757f
Abimbola Ronald <r.abimbola@alustudent.com>
2023-07-30 08:36:00 -0700

    Task 5

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Write a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- You are not allowed to use the `DESCRIBE` or `EXPLAIN` statements
+SHOW CREATE TABLE `first_table`;
