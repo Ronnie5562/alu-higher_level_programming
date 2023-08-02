6fb6152ae172f729f1d8a7e9f3348ebcf45a03b1
Abimbola Ronald <r.abimbola@alustudent.com>
2023-08-02 11:21:40 -0700

    Task 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 USE `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `states` (
-    id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    state_id INT NOT NULL FOREIGN KEY(`state_id`) REFERENCES states(`id`),
+    id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL, FOREIGN KEY(`state_id`) REFERENCES states(`id`),
     name VARCHAR(256) NOT NULL
 );
