bbbd7e19733c9252df5d9b53af278438168079cd
Abimbola Ronald <r.abimbola@alustudent.com>
2023-08-02 10:35:10 -0700

    Task 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
 -- Write a script that creates the database hbtn_0d_usa and the table states on your MySQL server.
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `states` (id INT UNIQUE AUTO_INCREMENTED NOT NULL PRIMARY KEY, name VARCHAR(256));
+USE `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `states` (id INT AUTO_INCREMENTED NOT NULL PRIMARY KEY, name VARCHAR(256));
