0b0b6a67892260e5d06320e4d7a28b5d4ace2f7a
Abimbola Ronald <r.abimbola@alustudent.com>
2023-08-03 03:00:31 -0700

    Tasks

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -2,5 +2,5 @@
 SELECT tv_genres.name FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shows.title = "Dexter";
+WHERE tv_shows.title = "Dexter"
 ORDER BY tv_genres.name;
