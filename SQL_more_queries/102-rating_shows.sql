a0eb6a4011f1ae183c4844381db3442e359e6a88
Abimbola Ronald <r.abimbola@alustudent.com>
2023-08-03 05:39:50 -0700

    Task 19

---
+++ SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+-- Write a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+SELECT title, SUM(rate) AS total_rating
+  FROM tv_shows
+       INNER JOIN tv_show_ratings
+       ON tv_shows.id = tv_show_ratings.show_id
+ GROUP BY title
+ ORDER BY total_rating DESC;
