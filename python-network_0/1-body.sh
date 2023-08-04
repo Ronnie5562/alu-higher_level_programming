eafbdbc0b355cdd571584bc1bedfd764e7ac6824
Abimbola Ronald <r.abimbola@alustudent.com>
2023-08-04 05:37:22 -0700

    Tasks

---
+++ python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Sends a GET request to a URL, and displays the body of the response
+curl -sL "$1"
---
+++ python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# sends a DELETE request to the URL passed as the first argument
+curl -sX DELETE "$1"
