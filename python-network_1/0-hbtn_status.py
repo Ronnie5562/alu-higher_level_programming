e18ac8468e03d7d1472bae897efe0a979ec71c59
Abimbola Ronald <r.abimbola@alustudent.com>
2023-08-04 15:02:22 -0700

    Tasks

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,11 +6,10 @@
 import urllib.request
 
 
-if __name__ == "__main__":
-    request = urllib.request.Request("https://alu-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
