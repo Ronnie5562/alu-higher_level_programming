9c7183c36a02201a46b2a498ae72bdf7421f749a
Abimbola Ronald <r.abimbola@alustudent.com>
2023-08-11 07:27:24 -0700

    Task 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""_summary_
+This module returns an integer: the addition of a and b
+"""
+
+
+def add_integer(a, b=98):
+    """_summary_
+
+    Args:
+        a (_type_): _description_
+        b (int, optional): _description_. Defaults to 98.
+
+    Raises:
+        TypeError: __if a is neither an int or float object__
+        TypeError: __if b is neither an int or float object__
+
+    Returns:
+        _type_: __The addition of a and b__
+    """
+
+
+    if not (isinstance(a, int)) and not (isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if not (isinstance(b, int)) and not (isinstance(b, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
