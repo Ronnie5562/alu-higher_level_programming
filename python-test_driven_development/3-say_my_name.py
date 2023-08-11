a97c1b0a024fae8b8060b7ebe188bcc51b8ff640
Abimbola Ronald <r.abimbola@alustudent.com>
2023-08-11 15:11:30 -0700

    Task 2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -18,10 +18,10 @@ def say_my_name(first_name, last_name=""):
     Returns:
         _str_: _My name is {first_name} {last_name}_
     """
-    
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
- 
+
     print(f"My name is {first_name} {last_name}")
