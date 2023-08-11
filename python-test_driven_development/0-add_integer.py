5f94e21a627ac00a775a3fd8ddb40d7a7deb9e3f
Abimbola Ronald <r.abimbola@alustudent.com>
2023-08-11 09:06:46 -0700

    Task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -19,7 +19,6 @@ def add_integer(a, b=98):
         _type_: __The addition of a and b__
     """
 
-
     if not (isinstance(a, int)) and not (isinstance(a, float)):
         raise TypeError("a must be an integer")
     if not (isinstance(b, int)) and not (isinstance(b, float)):
