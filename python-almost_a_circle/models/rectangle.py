08693e809fe0393776788b4bf39d40c4ae0c73be
Abimbola Ronald <r.abimbola@alustudent.com>
2023-08-14 11:04:16 -0700

    Tasks

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -84,4 +84,4 @@ class Rectangle(Base):
     def __str__(self):
         """String representation of a rectangle instance"""
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - \
-                {self.__width}/{self.__height}"
+{self.__width}/{self.__height}"
