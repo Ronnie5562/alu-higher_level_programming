b9c312fb8a937cc8f38ed94b802f3773ac1ae989
Abimbola Ronald <r.abimbola@alustudent.com>
2023-08-14 09:44:46 -0700

    Task 1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module creates a base class for all other classes in this project
+"""
+
+
+class Base:
+    """
+    This class will be the “base” of all other classes in this project
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Base class constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            self.id = Base.__nb_objects + 1
