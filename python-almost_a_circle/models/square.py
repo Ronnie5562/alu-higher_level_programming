509c664fae4d38d162540e1b80540fe761759ace
Abimbola Ronald <r.abimbola@alustudent.com>
2023-08-16 08:53:24 -0700

    Task

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Write the class Square that inherits from Rectangle
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ This class defines the blueprint for a Square object """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ This func initializes a Square object"""
+        super()__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """The string representation of a Square object"""
+        print(f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
