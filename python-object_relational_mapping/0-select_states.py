374547354dc5e6c42b7a8ad5ef81dcc4f4cb4740
Abimbola Ronald <r.abimbola@alustudent.com>
2023-08-18 06:52:54 -0700

    Task

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,16 +6,10 @@ import MySQLdb
 import sys
 
 
-conn = MySQLdb.connect(
-    host='localhost',
-    db=sys.argv[3],
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    port=3306
-)
-
-with conn.cursor() as cur:
-    """Used context manager to automatically close the cursor"""
-    cur.execute('SELECT * FROM states ORDER BY states.id;')
-    [print(row) for row in cur.fetchall()]
-conn.close()
+if __name__ == '__main__':
+    conn = MySQLdb.connect(db=sys.argv[3], user=sys.argv[1], passwd=sys.argv[2])
+    with conn.cursor() as cur:
+        """Used context manager to automatically close the cursor object"""
+        cur.execute('SELECT * FROM states ORDER BY states.id;')
+        [print(row) for row in cur.fetchall()]
+    conn.close()
