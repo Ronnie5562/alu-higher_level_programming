00d0779459b69714b3d760abf92091b3b332c819
Abimbola Ronald <r.abimbola@alustudent.com>
2023-08-18 07:49:26 -0700

    Task

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """
-displays all values in the states table where name matches the argument.
+Write a script that lists all states from the database hbtn_0e_4_usa
 """
 import MySQLdb
 import sys
+
 
 if __name__ == '__main__':
     con = MySQLdb.connect(db=sys.argv[3], user=sys.argv[1], passwd=sys.argv[2])
     with con.cursor() as cur:
         """Used context manager to automatically close the cursor object"""
-        query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id;"
-        cur.execute(query.format(sys.argv[4]))
+        cur.execute('''SELECT cities.id, cities.name, states.name FROM
+                    cities JOIN states ON states.id=cities.state_id;''')
         [print(row) for row in cur.fetchall()]
     con.close()
