f115468fbabedff5f29be36e0188bd102c9c24e1
Abimbola Ronald <r.abimbola@alustudent.com>
2023-08-18 02:36:45 -0700

    Task

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -8,7 +8,14 @@ from models.square import Square
 
 class TestBase(unittest.Testcase):
     """Test creation of a base instance"""
-    def test_no_id(self):
+    def test_none_id(self):
+        x = Base()
+        self.assertTrue(x.id is not None)
+
+    def test_prev_id(self):
         x = Base()
         y = Base()
         self.assertEqual(y.id, x.id + 1)
+
+if __name__ == "__main__":
+    unittest.main()
