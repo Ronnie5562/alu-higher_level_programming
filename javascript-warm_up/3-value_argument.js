48cc3ab114670ef9109dee4fb167f08a72bdb032
Ronald Abimbola <r.abimbola@alustudent.com>
2023-09-30 09:20:55 -0700

    Task

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const argv = process.argv.slice(2);
-const message = argv.length > 0 ? argv[0] : 'No argument';
+const message = argv[0] ? argv[0] : 'No argument';
 console.log(message);
