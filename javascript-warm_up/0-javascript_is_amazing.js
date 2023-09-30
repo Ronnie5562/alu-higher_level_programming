483feec9257ba511782caeda2b30d32f931b30a3
Ronald Abimbola <r.abimbola@alustudent.com>
2023-09-30 08:00:04 -0700

    Task

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-let myVar = "JavaScript is amazing"
-console.log(myVar)
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,2 @@
 #!/usr/bin/node
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+['C is fun', 'Python is cool', 'JavaScript is amazing'].forEach(str => console.log(str));
