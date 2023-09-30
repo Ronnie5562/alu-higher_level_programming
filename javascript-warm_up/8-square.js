bd8f0930ec0599b01040051bd83a223648a1472e
Ronald Abimbola <r.abimbola@alustudent.com>
2023-09-30 10:57:54 -0700

    Task

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
-const size = parseInt(process.argv[2]);
-if (isNaN(size)) console.log('Missing size');
-else if (size > 0) console.log(('X'.repeat(size) + '\n').repeat(size).trim());
-else console.log('Size must be a positive integer');
+const size = process.argv[2];
+
+if (!isNaN(size)) {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+} else {
+  console.log('Missing size');
+}
