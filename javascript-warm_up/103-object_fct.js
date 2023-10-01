816ddb44beeba6dfaa76c5817fad4afee6b2c4d4
Ronald Abimbola <r.abimbola@alustudent.com>
2023-09-30 18:02:28 -0700

    Task

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -4,9 +4,9 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-/*
-YOUR CODE HERE
-*/
+myObject.incr = () => {
+  myObject.value++;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
