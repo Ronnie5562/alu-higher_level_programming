dd24eae72a0399b9ee87e6396bac7473ed3e260d
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-10-08 20:17:23 +0200

    Task

--- javascript-web_jquery/7-script.js
+++ javascript-web_jquery/7-script.js
@@ -1,4 +1,4 @@
-// JS to fetches and replace character with URL
+// fetches a character name with an API 
 $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data) {
     $('#character').text(data.name);
 });
---
+++ javascript-web_jquery/8-script.js
@@ -0,0 +1,4 @@
+// script that fetches and lists the title with an API
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+    $('ul#list_movies').append(data.results.map(film => `<li>${film.title}</li>`));
+});
